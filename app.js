7f6e7bb62fb757c2f000fe6fd86c74a64d5b02df
lee-tae-eon <mktears0609@gmail.com>
2021-03-21 10:12:47 +0900

    complete

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d"); // getContext()를 사용해 픽셀값에 접근
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "black";
+const CANVAS_SIZE = 500;
 
 // 캔버스 element에 사이즈를 줘야함
-canvas.width = 700;
+canvas.width = 500;
 canvas.height = 500;
 
 // 시작 색상, 크기
-ctx.strokeStyle = "black";
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
-// flag
+
+// Drawing flag
 let drawing = false;
+// Fill mode flag
+let filling = false;
 
 function stopDrawing() {
   drawing = false;
@@ -33,14 +46,54 @@ function onMouseMove(event) {
   }
 }
 // 클릭시 마우스 이벤트
-function onMouseDown(event) {
-  // console.log(event);
-  drawing = true;
-}
+// function onMouseDown(event) {
+//   // console.log(event);
+//   drawing = true;
+// }
 // 마우스 클릭 해제시 이벤트
-function onMouseUp(event) {
-  // console.log(event);
-  stopDrawing();
+// function onMouseUp(event) {
+//   // console.log(event);
+//   stopDrawing();
+// }
+
+function changeColor(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color; // 기본색상을 클릭한 색상으로 변경
+  ctx.fillStyle = color;
+}
+// range bar
+function handleRangeChange(event) {
+  const rsize = event.target.value;
+  ctx.lineWidth = rsize;
+}
+// fill & drawing 모드
+function handleFillClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Drawing";
+  }
+}
+// 캔버스에 채우기(사이즈)
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+// 우클릭 방지
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick(event) {
+  const image = canvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "Picture";
+  //   console.log(link);
+  link.click();
 }
 
 if (canvas) {
@@ -48,4 +101,27 @@ if (canvas) {
   canvas.addEventListener("mousedown", startDrawing);
   canvas.addEventListener("mouseup", stopDrawing);
   canvas.addEventListener("mouseleave", stopDrawing);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
+}
+
+// console.log(Array.from(colors));
+
+// 색상을 배열로 만들어주고 각각을 클릭시 changeColor함수 실행
+if (colors) {
+  Array.from(colors).forEach((color) =>
+    color.addEventListener("click", changeColor)
+  );
+}
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleFillClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
